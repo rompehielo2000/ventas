7626d4349d83fa491d58aab2a6c70d254bd334af
ABRAHAM\ander <andersoncorreatorres@gmail.com>
2024-08-26 23:26:16 -0500

    eh agregado un comentario en el archivo main.py

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from Vista.ventanaProductos import VentanaProductos
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    # ejecuta la ventana ventas de productos
     window = VentanaProductos()
     app.exec() 
